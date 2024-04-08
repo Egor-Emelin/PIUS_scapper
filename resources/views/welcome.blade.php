0f2935ce43b6e0254312afd80ef7f5506db59993
egorik <eg.emelin2016@yandex.ru>
2024-04-08 10:12:50 +0300

    my commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
 
 <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
     @csrf
-    <input type="file" name="file">
     <button type="submit">Upload</button>
 </form>
 
